613b0c7c7cf75d58b9a895e88edb1acf49f9e184
Nazim Akash <69462083+nazimakash6@users.noreply.github.com>
2023-08-16 20:46:06 +0500

    getvalarr.js (get value from an array , case sensitive)
    Through this code, you will be able to find any value from an array using a prompt or input. This is case-sensitive.

---
+++ getvalarr.js
@@ -0,0 +1,6 @@
+ const input = prompt('Enter name in lowercase') //prompt box
+
+    const arr = ['nazim','akash','ijaz','zain']
+
+    // usign ternary operator
+    arr.includes(input) ? console.log(`Your name (${input}) has been recorded.`) : console.log('Record Not Found')
